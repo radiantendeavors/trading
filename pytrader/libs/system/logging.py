39b9bc41e62d85fa91a0271e81477a3523091720
G Derber <gd.github@radiantendeavors.com>
2022-06-20 16:52:38 -0400

    Fix header comments

--- pytrader/libs/system/logging.py
+++ pytrader/libs/system/logging.py
@@ -1,32 +1,10 @@
 #!/usr/bin/env python3
 # ==================================================================================================
 #
-# Original BASH version
-# Original version Copyright 2001 by Kyle Sallee
-# Additions/corrections Copyright 2002 by the Source Mage Team
-#
-# Python rewrite
-# Copyright 2017 Geoff S Derber
-#
-# This file is part of Sorcery.
-#
-#    Sorcery is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Dionysius is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Dionysius.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Logging:
 #
-#    Provides additional functionality to the Logging library from
-#    Python.
+#    Provides additional functionality to the Logging library from Python.
 #
 # ==================================================================================================
 
