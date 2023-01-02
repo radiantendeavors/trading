1f840459c0ffef9f50351de9a02c1cfd10d5d81c
G Derber <gd.github@radiantendeavors.com>
2023-01-02 16:33:30 -0500

    Add yahoo tables

---
+++ pytrader/libs/clients/database/yahoo_etf_dividends.py
@@ -0,0 +1,64 @@
+"""!
+@package pytrader.libs.clients.database
+
+Provides the database client
+
+@author Geoff S. derber
+@version HEAD
+@date 2022
+@copyright GNU Affero General Public License
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+@file pytrader/libs/clients/database/etf_bar_daily_raw.py
+"""
+from sqlalchemy import BigInteger, Boolean, Column, Date, Float, Integer, String
+
+# 3rd Party Libraries
+
+# Application Libraries
+# System Library Overrides
+from pytrader.libs.system import logging
+
+# Other Application Libraries
+from pytrader.libs.clients.database import Base
+
+# ==================================================================================================
+#
+# Global Variables
+#
+# ==================================================================================================
+"""!
+@var logger
+The base logger.
+
+@var colortext
+Allows Color text on the console
+"""
+logger = logging.getLogger(__name__)
+
+
+# ==================================================================================================
+#
+# Classes
+#
+# ==================================================================================================
+class YahooEtfDividends(Base):
+    __tablename__ = "yahoo_etf_dividends"
+    id = Column(Integer, Primary=True)
+    ticker = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    dividend = Column(Float, nullable=True, default=None)
+    date_downloaded = Column(Date)
