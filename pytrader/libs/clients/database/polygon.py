6cd128a2caa01051f0754eaa39a7caa8e416653c
Geoff S. Derber <geoff@drbr.org>
2023-01-20 16:26:31 -0500

    Add more tables for polygon data

--- pytrader/libs/clients/database/polygon.py
+++ pytrader/libs/clients/database/polygon.py
@@ -71,6 +71,54 @@ DBSession = scoped_session(sessionmaker())
 # Classes
 #
 # ==================================================================================================
+class PolygonCommonStockInfo(Base):
+    __tablename__ = "z_polygon_common_stock_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonCommonStockDetails(Base):
+    __tablename__ = "z_polygon_common_stock_details"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_common_stock_info.id"))
+    stock = relationship(PolygonCommonStockInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonCommonStockDelisted(Base):
+    __tablename__ = "z_polygon_common_stock_delisted"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_common_stock_info.id"))
+    stock = relationship(PolygonCommonStockInfo)
+    delisted_utc = Column(DateTime)
+
+
 class PolygonEtfInfo(Base):
     __tablename__ = "z_polygon_etf_info"
     id = Column(Integer, primary_key=True)
@@ -83,7 +131,6 @@ class PolygonEtfInfo(Base):
     currency_symbol = Column(String(6))
     base_currency_symbol = Column(String(6))
     base_currency_name = Column(String(6))
-    delisted_utc = Column(DateTime)
     last_updated_utc = Column(DateTime)
     locale = Column(String(6))
     market = Column(String(6))
@@ -94,28 +141,18 @@ class PolygonEtfInfo(Base):
 class PolygonEtfDetails(Base):
     __tablename__ = "z_polygon_etf_details"
     id = Column(Integer, primary_key=True)
-    ticker_symbol = Column(String(6))
-    active = Column(Boolean)
+    etf_id = Column(Integer, foreign_key=("z_polygon_etf_info.id"))
+    etf = relationship(PolygonEtfInfo)
     address = Column(String(128))
-    base_currency_name = Column(String(6))
-    base_currency_symbol = Column(String(6))
     branding = Column(String(128))
-    cik = Column(Integer)
     company_name = Column(String(64))
     composite_figi = Column(String(12))
-    currency_name = Column(String(6))
-    currency_symbol = Column(String(6))
     delisted_utc = Column(DateTime)
     description = Column(Text)
     homepage_url = Column(String(128))
-    last_updated_utc = Column(DateTime)
     list_date = Column(DateTime)
-    locale = Column(String(6))
-    market = Column(String(6))
     market_cap = Column(Float)
     phone_number = Column(String(12))
-    primary_exchange = Column(String(6))
-    share_class_figi = Column(String(12))
     share_class_shares_outstanding = Column(BigInteger)
     sic_code = Column(String(12))
     sic_description = Column(String(64))
@@ -124,8 +161,16 @@ class PolygonEtfDetails(Base):
     total_employees = Column(Integer)
 
 
-class PolygonStockInfo(Base):
-    __tablename__ = "z_polygon_stock_info"
+class PolygonEtfDelisted(Base):
+    __tablename__ = "z_polygon_etf_delisted"
+    id = Column(Integer, primary_key=True)
+    etf_id = Column(Integer, foreign_key=("z_polygon_etf_info.id"))
+    etf = relationship(PolygonEtfInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonEtnInfo(Base):
+    __tablename__ = "z_polygon_etn_info"
     id = Column(Integer, primary_key=True)
     ticker_symbol = Column(String(6))
     company_name = Column(String(64))
@@ -136,7 +181,6 @@ class PolygonStockInfo(Base):
     currency_symbol = Column(String(6))
     base_currency_symbol = Column(String(6))
     base_currency_name = Column(String(6))
-    delisted_utc = Column(DateTime)
     last_updated_utc = Column(DateTime)
     locale = Column(String(6))
     market = Column(String(6))
@@ -144,37 +188,281 @@ class PolygonStockInfo(Base):
     share_class_figi = Column(String(12))
 
 
-class PolygonStockDetails(Base):
-    __tablename__ = "z_polygon_stock_details"
+class PolygonEtnDetails(Base):
+    __tablename__ = "z_polygon_etn_details"
     id = Column(Integer, primary_key=True)
-    ticker_symbol = Column(String(6))
-    active = Column(Boolean)
+    etn_id = Column(Integer, foreign_key=("z_polygon_etn_info.id"))
+    etn = relationship(PolygonEtnInfo)
     address = Column(String(128))
-    base_currency_name = Column(String(6))
-    base_currency_symbol = Column(String(6))
     branding = Column(String(128))
-    cik = Column(Integer)
     company_name = Column(String(64))
     composite_figi = Column(String(12))
-    currency_name = Column(String(6))
-    currency_symbol = Column(String(6))
     delisted_utc = Column(DateTime)
     description = Column(Text)
     homepage_url = Column(String(128))
-    last_updated_utc = Column(DateTime)
     list_date = Column(DateTime)
-    locale = Column(String(6))
-    market = Column(String(6))
     market_cap = Column(Float)
     phone_number = Column(String(12))
-    primary_exchange = Column(String(6))
-    share_class_figi = Column(String(12))
     share_class_shares_outstanding = Column(BigInteger)
     sic_code = Column(String(12))
     sic_description = Column(String(64))
     ticker_root = Column(String(6))
     weighted_shares_outstanding = Column(BigInteger)
     total_employees = Column(Integer)
+
+
+class PolygonEtnDelisted(Base):
+    __tablename__ = "z_polygon_etn_delisted"
+    id = Column(Integer, primary_key=True)
+    etn_id = Column(Integer, foreign_key=("z_polygon_etn_info.id"))
+    etn = relationship(PolygonEtnInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonEtsInfo(Base):
+    __tablename__ = "z_polygon_ets_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonEtsDetails(Base):
+    __tablename__ = "z_polygon_ets_details"
+    id = Column(Integer, primary_key=True)
+    ets_id = Column(Integer, foreign_key=("z_polygon_ets_info.id"))
+    ets = relationship(PolygonEtsInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    composite_figi = Column(String(12))
+    delisted_utc = Column(DateTime)
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonEtsDelisted(Base):
+    __tablename__ = "z_polygon_ets_delisted"
+    id = Column(Integer, primary_key=True)
+    ets_id = Column(Integer, foreign_key=("z_polygon_ets_info.id"))
+    ets = relationship(PolygonEtsInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonEtvInfo(Base):
+    __tablename__ = "z_polygon_etv_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonEtvDetails(Base):
+    __tablename__ = "z_polygon_etv_details"
+    id = Column(Integer, primary_key=True)
+    etv_id = Column(Integer, foreign_key=("z_polygon_etv_info.id"))
+    etv = relationship(PolygonEtvInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    composite_figi = Column(String(12))
+    delisted_utc = Column(DateTime)
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonEtvDelisted(Base):
+    __tablename__ = "z_polygon_etv_delisted"
+    id = Column(Integer, primary_key=True)
+    etv_id = Column(Integer, foreign_key=("z_polygon_etv_info.id"))
+    etv = relationship(PolygonEtvInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonIndexInfo(Base):
+    __tablename__ = "z_polygon_index_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonIndexDetails(Base):
+    __tablename__ = "z_polygon_index_details"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_index_info.id"))
+    stock = relationship(PolygonIndexInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonIndexDelisted(Base):
+    __tablename__ = "z_polygon_index_delisted"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_index_info.id"))
+    stock = relationship(PolygonIndexInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonPreferredStockInfo(Base):
+    __tablename__ = "z_polygon_preferred_stock_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonPreferredStockDetails(Base):
+    __tablename__ = "z_polygon_preferred_stock_details"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer,
+                      foreign_key=("z_polygon_preferred_stock_info.id"))
+    stock = relationship(PolygonPreferredStockInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonPreferredStockDelisted(Base):
+    __tablename__ = "z_polygon_preferred_stock_delisted"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer,
+                      foreign_key=("z_polygon_preferred_stock_info.id"))
+    stock = relationship(PolygonPreferredStockInfo)
+    delisted_utc = Column(DateTime)
+
+
+class PolygonWarrantInfo(Base):
+    __tablename__ = "z_polygon_warrant_info"
+    id = Column(Integer, primary_key=True)
+    ticker_symbol = Column(String(6))
+    company_name = Column(String(64))
+    active = Column(Boolean)
+    cik = Column(Integer)
+    composite_figi = Column(String(12))
+    currency_name = Column(String(6))
+    currency_symbol = Column(String(6))
+    base_currency_symbol = Column(String(6))
+    base_currency_name = Column(String(6))
+    last_updated_utc = Column(DateTime)
+    locale = Column(String(6))
+    market = Column(String(6))
+    primary_exchange = Column(String(6))
+    share_class_figi = Column(String(12))
+
+
+class PolygonWarrantDetails(Base):
+    __tablename__ = "z_polygon_warrant_details"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_warrant_info.id"))
+    stock = relationship(PolygonWarrantInfo)
+    address = Column(String(128))
+    branding = Column(String(128))
+    company_name = Column(String(64))
+    description = Column(Text)
+    homepage_url = Column(String(128))
+    list_date = Column(DateTime)
+    market_cap = Column(Float)
+    phone_number = Column(String(12))
+    share_class_shares_outstanding = Column(BigInteger)
+    sic_code = Column(String(12))
+    sic_description = Column(String(64))
+    ticker_root = Column(String(6))
+    weighted_shares_outstanding = Column(BigInteger)
+    total_employees = Column(Integer)
+
+
+class PolygonWarrantDelisted(Base):
+    __tablename__ = "z_polygon_warrant_delisted"
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(Integer, foreign_key=("z_polygon_warrant_info.id"))
+    stock = relationship(PolygonWarrantInfo)
+    delisted_utc = Column(DateTime)
 
 
 # ==================================================================================================
