36046a7404175b82adca3269f50a0a220d83c410
G Derber <gd.github@radiantendeavors.com>
2023-03-09 14:24:24 -0500

    Add abstract methods that can be later defined by actual strategies

--- pytrader/strategies/__init__.py
+++ pytrader/strategies/__init__.py
@@ -28,10 +28,11 @@ Provides the Base Class for a Strategy.
 # System libraries
 import datetime
 
+from abc import ABCMeta, abstractmethod
+
 # 3rd Party libraries
 import pandas
 from ibapi import contract
-from ibapi import order
 
 # System Library Overrides
 from pytrader.libs.system import logging
@@ -61,20 +62,30 @@ logger = logging.getLogger(__name__)
 # ==================================================================================================
 class Strategy():
 
+    __metaclass__ = ABCMeta
+
     def __init__(self, brokerclient):
         self.brokerclient = brokerclient
 
         self.endtime = datetime.datetime.combine(
             datetime.date.today(), datetime.time(hour=16, minute=16))
         self.time_now = datetime.datetime.now()
-        self.real_time_bars = pandas.DataFrame(columns=[
-            "DateTime", "Open", "High", "Low", "Close", "Volume", "Count"
-        ])
+        self.bars = []
         ## Position Status: -1 = short, 0 = Empty, 1 = long
         self.position_status = 0
 
+    @abstractmethod
+    def on_5sec_rtb(self, real_time_bar):
+        pass
+
+    @abstractmethod
+    def on_bar(self):
+        pass
+
     def run(self):
         logger.debug10("Begin Function")
+
+        bar_adjustment = self._bar_conversion()
 
         self.contract = contract.Contract()
         self.contract.symbol = self.security
@@ -88,8 +99,6 @@ class Strategy():
 
         bar_list = self.brokerclient.get_data(req_id)
 
-        adj_bar_list = []
-
         for bar in bar_list:
             logger.debug3("Bar: %s", bar)
             bar_date = bar.date
@@ -98,106 +107,70 @@ class Strategy():
             bar_low = bar.low
             bar_close = bar.close
             bar_volume = bar.volume
-            bar_wap = bar.wap
             bar_count = bar.barCount
 
-            adj_bar_list.append([
+            self.bars.append([
                 bar_date, bar_open, bar_high, bar_low, bar_close, bar_volume,
-                bar_wap, bar_count
+                bar_count
             ])
-
-        self.bars = pandas.DataFrame(adj_bar_list,
-                                     columns=[
-                                         "DateTime", "Open", "High", "Low",
-                                         "Close", "Volume", "WAP", "Count"
-                                     ])
-        self.bars["DateTime"] = pandas.to_datetime(self.bars["DateTime"],
-                                                   format="%Y%m%d %H:%M:%S %Z")
-
-        short_name = str(self.short_period) + "EMA"
-        self.bars[short_name] = self.bars["Close"].ewm(span=self.short_period,
-                                                       adjust=False).mean()
-
-        long_name = str(self.long_period) + "EMA"
-        self.bars[long_name] = self.bars["Close"].ewm(span=self.long_period,
-                                                      adjust=False).mean()
 
         req_id = self.brokerclient.req_real_time_bars(self.contract)
 
+        real_time_bars = []
+
         while self.time_now < self.endtime:
             real_time_bar = self.brokerclient.realtime_bar_queue[req_id].get()
-            logger.debug("Real Time Bar: %s", real_time_bar)
 
             bar_datetime = datetime.datetime.fromtimestamp(
                 real_time_bar[0]).strftime('%Y%m%d %H:%M:%S')
             bar_datetime_str = str(bar_datetime) + " EST"
-            logger.debug("Bar DateTime: %s", bar_datetime)
 
             real_time_bar[0] = bar_datetime_str
             real_time_bar[5] = int(real_time_bar[5])
             real_time_bar[6] = float(real_time_bar[6])
-            real_time_bar.append(0)
-            real_time_bar.append(0)
 
-            logger.debug("Bars: %s", self.bars.tail(5))
             logger.debug("Real Time Bar: %s", real_time_bar)
 
-            # FIXME: Should check that the bar is not already in the dataframe
-            # (only a problem when first starting)
-            self.bars.loc[len(self.bars)] = real_time_bar
+            real_time_bars.append(real_time_bar)
 
-            short_name = str(self.short_period) + "EMA"
-            self.bars[short_name] = self.bars["Close"].ewm(
-                span=self.short_period, adjust=False).mean()
+            logger.debug("Bar adjustment: %s", bar_adjustment)
+            if len(real_time_bars) == bar_adjustment:
+                rtb_date = real_time_bars[0][0]
+                rtb_open = real_time_bars[0][1]
+                rtb_high = max(l[2] for l in real_time_bars)
+                rtb_low = min(l[3] for l in real_time_bars)
+                rtb_close = real_time_bars[-1][4]
+                rtb_volumn = sum(l[5] for l in real_time_bars)
+                rtb_count = sum(l[6] for l in real_time_bars)
 
-            long_name = str(self.long_period) + "EMA"
-            self.bars[long_name] = self.bars["Close"].ewm(
-                span=self.long_period, adjust=False).mean()
+                new_bar = [
+                    rtb_date, rtb_open, rtb_high, rtb_low, rtb_close,
+                    rtb_volumn, rtb_count
+                ]
+                self.bars.append(new_bar)
 
-            logger.debug("Real Time Bar (with EMA): %s", self.bars.tail(20))
+                self.bars_df = pandas.DataFrame(self.bars,
+                                                columns=[
+                                                    "DateTime", "Open", "High",
+                                                    "Low", "Close", "Volume",
+                                                    "Count"
+                                                ])
+                self.bars_df["DateTime"] = pandas.to_datetime(
+                    self.bars_df["DateTime"], format="%Y%m%d %H:%M:%S %Z")
 
-            previous_short = self.bars[short_name].iloc[-2]
-            previous_long = self.bars[long_name].iloc[-2]
-
-            current_short = self.bars[short_name].iloc[-1]
-            current_long = self.bars[long_name].iloc[-1]
-
-            cross_down = ((current_short <= current_long) &
-                          (previous_short >= previous_long))
-
-            cross_up = ((current_short >= current_long) &
-                        (previous_short <= previous_long))
-            logger.debug("Cross Up: %s", cross_up)
-            logger.debug("Cross Down: %s", cross_down)
-
-            quantity = 100
-
-            if cross_up:
-                logger.info("EMA Cross Up")
-
-                if self.position_status == -1:
-                    quantity = quantity * 2
-
-                buy_order = order.Order()
-                buy_order.action = "BUY"
-                buy_order.totalQuantity = quantity
-                buy_order.orderType = "MKT"
-                self.brokerclient.place_order(self.contract, buy_order)
-                self.position_status = 1
-
-            if cross_down:
-                logger.info("EMA Cross Down")
-                if self.position_status == 1:
-                    quantity = quantity * 2
-
-                sell_order = order.Order()
-                sell_order.action = "SELL"
-                sell_order.totalQuantity = quantity
-                sell_order.orderType = "MKT"
-                self.brokerclient.place_order(self.contract, sell_order)
-                self.position_status = -1
+                self.on_bar()
+                real_time_bars = []
+            else:
+                try:
+                    self.on_5sec_rtb(real_time_bar)
+                except Exception as msg:
+                    logger.error("Exception: %s", msg)
 
         logger.debug10("End Function")
+
+    def _bar_conversion(self):
+        bar_conversion = {"5 secs": 1, "30 secs": 6, "1 min": 12, "5 mins": 60}
+        return bar_conversion[self.bar_sizes]
 
 
 # ==================================================================================================
