1433f91aca332ae383051587fe7bfe4bfce1eecf
G Derber <gd.github@radiantendeavors.com>
2023-04-18 10:05:48 -0400

    Remove port from config, this is now auto-detected

--- pytrader/libs/utilities/config/brokerconfig.py
+++ pytrader/libs/utilities/config/brokerconfig.py
@@ -37,16 +37,12 @@ class BrokerConfig():
 
     def __init__(self, *args, **kwargs):
         self.brokerclient_address = "127.0.0.1"
-        self.brokerclient_port = 7496
 
     def read_config(self, *args, **kwargs):
         config = kwargs["config"]
 
         if "brokerclient_address" in config:
             self.brokerclient_address = config["brokerclient_address"]
-
-        if "brokerclient_port" in config:
-            self.brokerclient_port = config["brokerclient_port"]
 
     def get_brokerclient_address(self):
         return self.brokerclient_address
